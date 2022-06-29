dd7e843fe7b5a528611538ac75fccf98f468fb10
Arman <arman.zarkalam@gmail.com>
2022-06-29 23:27:19 +0430

    Creating CheckRegularExpressions class

---
+++ Main/Main/CheckRegularExpressions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    internal class CheckRegularExpressions
+    {
+        //public static bool CheckEmailCorrection(string Email)
+        //{
+        //    if (!NormalUser.AllEmails.Contains(Email))
+        //    {
+        //        if (Regex.IsMatch(Email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$"))
+        //        {
+        //            return true;
+        //        }
+        //    }
+        //    MessageBox.Show("The entered email address is not in the correct format!");
+        //    return false;
+        //}
+        public static bool CheckEmailValidation(string Email)
+        {
+            return true;
+        }
+    }
+}
