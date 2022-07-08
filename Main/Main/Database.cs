63a4e8062fd7a9b8cf7dbb613998f63cca5fd186
amirali-lll <98775758+amirali-lll@users.noreply.github.com>
2022-07-08 16:21:34 +0430

    Update LoadBooks methods

--- Main/Main/Database.cs
+++ Main/Main/Database.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Data.Sql;
 
 
 
@@ -16,7 +11,7 @@ namespace Main
 {
     class Database
     {
-        
+
         public static void LoadAllBooks()
         {
             List<Book> books = new List<Book>();
@@ -28,17 +23,17 @@ namespace Main
             adapter.Fill(data);
             for (int i = 0; i < data.Rows.Count; i++)
             {
-                int id = (int)data.Rows[i][0];
-                string name = (string)data.Rows[i][1];
-                string AuthorName = (string)data.Rows[i][2];
-                int n_pages = (int)data.Rows[i][3];
-                string description = (string)data.Rows[i][4];
-                double cost = (float)data.Rows[i][5];
-                int discount_percen = (int)data.Rows[i][6];
-                int n_sells = (int)data.Rows[i][7];
-                string imageURL = (string)data.Rows[i][8];
-                string PDFURL = (string)data.Rows[i][9];
-                Book book = new Book(id, name, AuthorName, n_pages, description, cost, discount_percen, n_sells, imageURL, PDFURL);
+                int     id              = (int)     data.Rows[i][0];
+                string  name            = (string)  data.Rows[i][1];
+                string  AuthorName      = (string)  data.Rows[i][2];
+                int     n_pages         = (int)     data.Rows[i][3];
+                string  description     = (string)  data.Rows[i][4];
+                double  cost            = (double)   data.Rows[i][5];
+                int     discount_percen = (int)     data.Rows[i][6];
+                int     n_sells         = (int)     data.Rows[i][7];
+                string  imageURL        = (string)  data.Rows[i][8];
+                string  PDFURL          = (string)  data.Rows[i][9];
+                Book    book            = new Book(id, name, AuthorName, n_pages, description, cost, discount_percen, n_sells, imageURL, PDFURL);
             }
 
             conn.Close();
@@ -51,14 +46,14 @@ namespace Main
             List<Manager> managers = new List<Manager>();
             SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\db.mdf;Integrated Security=True;Connect Timeout=30");
             conn.Open();
-            string         command = "SELECT * FROM Managers";
+            string command = "SELECT * FROM Managers";
             SqlDataAdapter adapter = new SqlDataAdapter(command, conn);
-            DataTable      data    = new DataTable();
+            DataTable data = new DataTable();
             adapter.Fill(data);
             for (int i = 0; i < data.Rows.Count; i++)
             {
-                string email    = (string) data.Rows[i][1];
-                string password = (string) data.Rows[i][2];
+                string email = (string)data.Rows[i][1];
+                string password = (string)data.Rows[i][2];
                 Manager manager = new Manager(email, password);
                 managers.Add(manager);
             }
@@ -66,10 +61,10 @@ namespace Main
             conn.Close();
             return managers;
         }
+
+
+
         
-
-
-
 
     }
 }
