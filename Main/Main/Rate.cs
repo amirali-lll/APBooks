74ba9e75297a8994f0e652660594b3606330f135
amirali-lll <98775758+amirali-lll@users.noreply.github.com>
2022-07-08 14:07:11 +0430

    initialize Rate.cs

---
+++ Main/Main/Rate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Rate
+    {
+        int amount;
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                if (value >= 0 && value <= 5)
+                    amount = value;
+                else
+                    throw new Exception("the rate must between 0-5");
+            }
+        }
+        public NormalUser user;
+
+        public Rate(int amount, NormalUser user)
+        {
+            Amount = amount;
+            this.user = user;
+        }
+    }
+}
