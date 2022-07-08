54e0ff02017ed8f13b8fdc37e521100fc5f1fe71
amirali-lll <98775758+amirali-lll@users.noreply.github.com>
2022-07-08 12:03:48 +0430

    Initialize Database class

---
+++ Main/Main/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//ToDo:
+
+//in data base Users : VIP and Cart remain.
+namespace Main
+{
+    class Database
+    {
+        //create all methods there
+
+
+
+
+
+    }
+}
